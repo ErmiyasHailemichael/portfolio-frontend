07f362c048863ad422f0b672b01a78b587bff7f5
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2022-12-21 02:30:49 -0800

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,27 @@
+import Home from "../pages/Home";
+import About from "../pages/About";
+
 function Footer(props) {
-    return <h1>Footer</h1>;
+    return (
+      <footer>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/">Twitter</a>
+          <a href="https://github.com/">Github</a>
+          <a href="https://instagram.com/">Instagram</a>
+        </li>
+      </ul>
+    </footer>
+    );
   }
   
   export default Footer;
