a678ed482e09fd6fd0f649abe182c6e885e5117a
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-01-21 01:23:05 -0800

    update on the frontend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,25 @@ import About from "../pages/About";
 
 function Footer(props) {
     return (
-      <footer>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/projects">Projects</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/">Twitter</a>
-          <a href="https://github.com/ErmiyasHailemichael">Github</a>
-          <a href="https://instagram.com/">Instagram</a>
-        </li>
-      </ul>
-    </footer>
+    //   <footer>
+    //   <ul>
+    //     <li>
+    //       <a href="/">Home</a>
+    //     </li>
+    //     <li>
+    //       <a href="/about">About</a>
+    //     </li>
+    //     <li>
+    //       <a href="/projects">Projects</a>
+    //     </li>
+    //     <li>
+    //       <a href="https://twitter.com/">Twitter</a>
+    //       <a href="https://github.com/ErmiyasHailemichael">Github</a>
+    //       <a href="https://instagram.com/">Instagram</a>
+    //     </li>
+    //   </ul>
+    // </footer>
+    <></>
     );
   }
   
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
 function Home(props) {
-    return <h1>Home</h1>;
-  }
-  
-  export default Home;
+  return (
+    <div className="home">
+      <img
+        className="profileimg"
+        src="https://i.imgur.com/oijcvW5.jpg"
+        alt="headshot"
+      />
+      <h1>Welcome, I'm Ermiyas!</h1>
+      <p>I am a software enginner student at GA</p>
+      <ul>Skills: </ul>
+      <li>CSS</li>
+      <li>HTML</li>
+      <li>React</li>
+      <li>JavaScript</li>
+    </div>
+  );
+}
+
+export default Home;
